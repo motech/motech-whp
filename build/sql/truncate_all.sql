c82c5fa7dc69ab1fd00e5f9c9accfc8e105a4703
Preethi <preethid@thoughtworks.com>
2012-04-05 14:07:45 +0530

    Banka, Preethi | #000 | Set up basic project structure

---
+++ build/sql/truncate_all.sql
@@ -0,0 +1,13 @@
+delete from QRTZ_BLOB_TRIGGERS;
+delete from QRTZ_CALENDARS;
+delete from QRTZ_CRON_TRIGGERS;
+delete from QRTZ_FIRED_TRIGGERS;
+delete from QRTZ_JOB_LISTENERS;
+delete from QRTZ_LOCKS;
+delete from QRTZ_PAUSED_TRIGGER_GRPS;
+delete from QRTZ_SCHEDULER_STATE;
+delete from QRTZ_SIMPLE_TRIGGERS;
+delete from QRTZ_TRIGGERS;
+delete from QRTZ_TRIGGER_LISTENERS;
+delete from QRTZ_JOB_DETAILS;
+
