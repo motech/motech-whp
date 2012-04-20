dac085b5334ff06a83959275591de3ed80b317fb
shruthi <shruthidipali@gmail.com>
2012-04-20 14:35:55 +0530

    Shruthi | #39 | Added 'Other' category to Gender.

--- whp-patient/src/main/java/org/motechproject/whp/patient/domain/Gender.java
+++ whp-patient/src/main/java/org/motechproject/whp/patient/domain/Gender.java
@@ -2,11 +2,23 @@ package org.motechproject.whp.patient.domain;
 
 public enum Gender {
 
-    Male, Female;
+    Male("M"), Female("F"), Other("O");
+
+    private String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
 
     public static Gender get(String gender) {
-        if ("M".equals(gender.toUpperCase())) return Male;
-        return Female;
+        if (Male.getValue().equals(gender.toUpperCase())) return Male;
+        if (Female.getValue().equals(gender.toUpperCase())) return Female;
+        if (Other.getValue().equals(gender.toUpperCase())) return Other;
+        return null;
+    }
+
+    public String getValue() {
+        return value;
     }
 
 }
