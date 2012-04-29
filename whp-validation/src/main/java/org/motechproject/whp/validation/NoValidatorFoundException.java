610fc238781d8195752164baa6a6bde6940bef6c
Balaji Narain <narainbalaji@gmail.com>
2012-04-29 11:32:32 +0530

    BalajiN | #0 | Removing whp-validation module

--- whp-validation/src/main/java/org/motechproject/whp/validation/NoValidatorFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.motechproject.whp.validation;
-
-
-public class NoValidatorFoundException extends RuntimeException {
-
-    public NoValidatorFoundException(String constraintName) {
-        super(String.format("%s %s", "No validator found for the constraint with name", constraintName));
-    }
-}
