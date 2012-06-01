2e3544b1fa977553630abc6f9665fe491e12189f
Balaji Narain <balajin@thoughtworks.com>
2012-06-01 17:01:21 +0530

    BalajiN | #0 | Supporting Indetermediate/Spoiled/Poor as a possible smear test result

--- whp-refdata/src/main/java/org/motechproject/whp/refdata/domain/SmearTestResult.java
+++ whp-refdata/src/main/java/org/motechproject/whp/refdata/domain/SmearTestResult.java
@@ -1,6 +1,17 @@
 package org.motechproject.whp.refdata.domain;
 
 public enum SmearTestResult {
-    Positive, Negative
+
+    Positive("Positive"), Negative("Negative"), Indetermediate("Indetermediate/Spoiled/Poor");
+
+    String value;
+
+    private SmearTestResult(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
 }
 
