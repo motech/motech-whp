16bb2e658f9be3b9e247a1688fb9ffcaa741b75b
Dheerendra Nath <dheeru@thoughtworks.com>
2012-06-01 13:01:33 +0530

    Dheeru | #109 | Added test for add(), latest() and coverage for multiple Instances in WeightStatisticsTest (created new test file)

--- whp-patient/src/test/java/org/motechproject/whp/patient/domain/WeightStatisticsTest.java
+++ whp-patient/src/test/java/org/motechproject/whp/patient/domain/WeightStatisticsTest.java
@@ -11,43 +11,77 @@ import static junit.framework.Assert.assertTrue;
 public class WeightStatisticsTest {
 
     @Test
-    public void shouldAddANewWeightResult_ForPreTreatment() {
-        WeightStatisticsRecord preTreatmentResults = new WeightStatisticsRecord(WeightInstance.PreTreatment, null, DateUtil.today());
-
+    public void shouldAddANewWeightResult_AllInstancesInOrder_shouldPreserveOrder() {
         WeightStatistics weightStatistics = new WeightStatistics();
-        weightStatistics.add(preTreatmentResults);
+        for(WeightInstance type : WeightInstance.values()) {
+            WeightStatisticsRecord weightStatisticsRecord = new WeightStatisticsRecord(type, null, DateUtil.today());
+            weightStatistics.add(weightStatisticsRecord);
+        }
 
-        assertEquals(1, weightStatistics.size());
+        assertEquals(5, weightStatistics.size());
         assertTrue(weightStatistics.get(0).isOfInstance(WeightInstance.PreTreatment));
+        assertTrue(weightStatistics.get(1).isOfInstance(WeightInstance.EndIP));
+        assertTrue(weightStatistics.get(2).isOfInstance(WeightInstance.ExtendedIP));
+        assertTrue(weightStatistics.get(3).isOfInstance(WeightInstance.TwoMonthsIntoCP));
+        assertTrue(weightStatistics.get(4).isOfInstance(WeightInstance.EndTreatment));
     }
 
     @Test
-    public void shouldAddWeightResults_ForBothPreTreatment_AndEndTreatment() {
-        WeightStatisticsRecord preTreatmentResults = new WeightStatisticsRecord(WeightInstance.PreTreatment, null, DateUtil.today());
-        WeightStatisticsRecord endTreatmentResults = new WeightStatisticsRecord(WeightInstance.EndTreatment, null, DateUtil.today());
-
+    public void shouldUpdateCurrentWeightResult_AllInstancesInOrder_shouldPreserveOrder() {
         WeightStatistics weightStatistics = new WeightStatistics();
-        weightStatistics.add(preTreatmentResults);
-        weightStatistics.add(endTreatmentResults);
+        for(WeightInstance type : WeightInstance.values()) {
+            WeightStatisticsRecord oldWeightStatisticsRecord = new WeightStatisticsRecord(type, null, new LocalDate(2010, 10, 10));
+            weightStatistics.add(oldWeightStatisticsRecord);
+        }
 
+        LocalDate newMeasuringDate = new LocalDate(2010, 12, 12);
+        for(WeightInstance type : WeightInstance.values()) {
+            WeightStatisticsRecord newWeightStatisticsRecord = new WeightStatisticsRecord(type, null, newMeasuringDate);
+            weightStatistics.add(newWeightStatisticsRecord);
+        }
+
+        assertEquals(5, weightStatistics.size());
+        assertTrue(weightStatistics.get(0).getMeasuringDate().equals(newMeasuringDate));
+        assertTrue(weightStatistics.get(1).getMeasuringDate().equals(newMeasuringDate));
+        assertTrue(weightStatistics.get(2).getMeasuringDate().equals(newMeasuringDate));
+        assertTrue(weightStatistics.get(3).getMeasuringDate().equals(newMeasuringDate));
+        assertTrue(weightStatistics.get(4).getMeasuringDate().equals(newMeasuringDate));
+    }
+
+    @Test
+    public void shouldAddWeightStatisticsAsLatest_WhenStatisticsAreForDifferentInstance() {
+        WeightStatistics weightStatistics = new WeightStatistics();
+        weightStatistics.add(new WeightStatisticsRecord(WeightInstance.PreTreatment, null, new LocalDate(2010, 10, 10)));
+        assertEquals(1, weightStatistics.size());
+        assertTrue(weightStatistics.latestResult().isOfInstance(WeightInstance.PreTreatment));
+        weightStatistics.add(new WeightStatisticsRecord(WeightInstance.EndIP, null, new LocalDate(2010, 10, 10)));
         assertEquals(2, weightStatistics.size());
-        assertTrue(weightStatistics.get(0).isOfInstance(WeightInstance.PreTreatment));
-        assertTrue(weightStatistics.get(1).isOfInstance(WeightInstance.EndTreatment));
+        assertTrue(weightStatistics.latestResult().isOfInstance(WeightInstance.EndIP));
+        weightStatistics.add(new WeightStatisticsRecord(WeightInstance.ExtendedIP, null, new LocalDate(2010, 10, 10)));
+        assertEquals(3, weightStatistics.size());
+        assertTrue(weightStatistics.latestResult().isOfInstance(WeightInstance.ExtendedIP));
+        weightStatistics.add(new WeightStatisticsRecord(WeightInstance.TwoMonthsIntoCP, null, new LocalDate(2010, 10, 10)));
+        assertEquals(4, weightStatistics.size());
+        assertTrue(weightStatistics.latestResult().isOfInstance(WeightInstance.TwoMonthsIntoCP));
+        weightStatistics.add(new WeightStatisticsRecord(WeightInstance.EndTreatment, null, new LocalDate(2010, 10, 10)));
+        assertEquals(5, weightStatistics.size());
+        assertTrue(weightStatistics.latestResult().isOfInstance(WeightInstance.EndTreatment));
     }
 
     @Test
-    public void shouldUpdateCurrentWeightResult_ForPreTreatment() {
-        WeightStatisticsRecord oldPreTreatmentResults = new WeightStatisticsRecord(WeightInstance.PreTreatment, null, new LocalDate(2010, 10, 10));
-        WeightStatisticsRecord newPreTreatmentResults = new WeightStatisticsRecord(WeightInstance.PreTreatment, null, new LocalDate(2012, 12, 12));
+    public void shouldReplaceWeightStatisticsAsLatest_WhenStatisticsAreRecentForSameInstance() {
+        for(WeightInstance type : WeightInstance.values()) {
+            verifyFor(type);
+        }
+    }
 
+    private void verifyFor(WeightInstance type) {
         WeightStatistics weightStatistics = new WeightStatistics();
-
-        weightStatistics.add(oldPreTreatmentResults);
+        weightStatistics.add(new WeightStatisticsRecord(type, null, new LocalDate(2010, 10, 10)));
         assertEquals(1, weightStatistics.size());
-        assertEquals(oldPreTreatmentResults, weightStatistics.get(0));
-
-        weightStatistics.add(newPreTreatmentResults);
+        assertTrue(weightStatistics.latestResult().isOfInstance(type));
+        weightStatistics.add(new WeightStatisticsRecord(type, null, new LocalDate(2010, 12, 12)));
         assertEquals(1, weightStatistics.size());
-        assertEquals(newPreTreatmentResults, weightStatistics.get(0));
+        assertTrue(weightStatistics.latestResult().isOfInstance(type));
     }
 }
