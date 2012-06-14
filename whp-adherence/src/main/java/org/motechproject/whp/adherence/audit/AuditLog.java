0b697f29f0a1ab8e87980daf6d6b671239d337df
unknown <srivathr@.corporate.thoughtworks.com>
2012-06-14 18:30:36 +0530

    <Sri> #000 | Adding creation time to audit logs.

--- whp-adherence/src/main/java/org/motechproject/whp/adherence/audit/AuditLog.java
+++ whp-adherence/src/main/java/org/motechproject/whp/adherence/audit/AuditLog.java
@@ -3,11 +3,15 @@ package org.motechproject.whp.adherence.audit;
 
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.ektorp.support.TypeDiscriminator;
+import org.joda.time.DateTime;
 import org.motechproject.model.MotechBaseDataObject;
+import org.motechproject.util.DateUtil;
 
 @TypeDiscriminator("doc.type == 'AuditLog'")
 public class AuditLog extends MotechBaseDataObject {
 
+    @JsonProperty
+    private DateTime creationTime = DateUtil.now();
     @JsonProperty
     private int numberOfDosesTaken;
     @JsonProperty
@@ -84,5 +88,47 @@ public class AuditLog extends MotechBaseDataObject {
 
     public String user() {
         return user;
+    }
+
+    public DateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(DateTime creationTime) {
+        this.creationTime = DateUtil.setTimeZone(creationTime);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AuditLog auditLog = (AuditLog) o;
+
+        if (numberOfDosesTaken != auditLog.numberOfDosesTaken) return false;
+        if (creationTime != null ? !creationTime.equals(auditLog.creationTime) : auditLog.creationTime != null)
+            return false;
+        if (patientId != null ? !patientId.equals(auditLog.patientId) : auditLog.patientId != null) return false;
+        if (providerId != null ? !providerId.equals(auditLog.providerId) : auditLog.providerId != null) return false;
+        if (remark != null ? !remark.equals(auditLog.remark) : auditLog.remark != null) return false;
+        if (sourceOfChange != null ? !sourceOfChange.equals(auditLog.sourceOfChange) : auditLog.sourceOfChange != null)
+            return false;
+        if (tbId != null ? !tbId.equals(auditLog.tbId) : auditLog.tbId != null) return false;
+        if (user != null ? !user.equals(auditLog.user) : auditLog.user != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = creationTime != null ? creationTime.hashCode() : 0;
+        result = 31 * result + numberOfDosesTaken;
+        result = 31 * result + (remark != null ? remark.hashCode() : 0);
+        result = 31 * result + (sourceOfChange != null ? sourceOfChange.hashCode() : 0);
+        result = 31 * result + (patientId != null ? patientId.hashCode() : 0);
+        result = 31 * result + (tbId != null ? tbId.hashCode() : 0);
+        result = 31 * result + (providerId != null ? providerId.hashCode() : 0);
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        return result;
     }
 }
---
+++ whp-adherence/src/test/java/org/motechproject/whp/adherence/audit/AllAuditLogsTest.java
@@ -0,0 +1,49 @@
+package org.motechproject.whp.adherence.audit;
+
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Test;
+import org.motechproject.whp.patient.repository.SpringIntegrationTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@ContextConfiguration(locations = "classpath*:/applicationWHPAdherenceContext.xml")
+public class AllAuditLogsTest extends SpringIntegrationTest {
+
+    @Autowired
+    AllAuditLogs allAuditLogs;
+
+    @Test
+    public void shouldLogAuditWithCreationTime() {
+        List<AuditLog> auditLogs = allAuditLogs.getAll();
+        assertEquals(0, auditLogs.size());
+
+        AuditLog auditLog = new AuditLog()
+                .numberOfDosesTaken(1)
+                .providerId("raj")
+                .remark("dose taken")
+                .user("raj")
+                .sourceOfChange("WEB")
+                .patientId("aha0100009")
+                .tbId("elevenDigit");
+
+        DateTime creationTime = auditLog.getCreationTime();
+        assertNotNull(creationTime);
+
+        allAuditLogs.add(auditLog);
+
+        auditLogs = allAuditLogs.getAll();
+        assertEquals(creationTime, auditLogs.get(0).getCreationTime());
+        assertTrue(auditLog.equals(auditLogs.get(0)));
+    }
+
+    @After
+    public void tearDown(){
+        super.after();
+        markForDeletion(allAuditLogs.getAll().toArray());
+    }
+}
