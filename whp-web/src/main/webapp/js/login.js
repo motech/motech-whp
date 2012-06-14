f8df7782e7905bce729003d68bb6bd18bf999b90
shruthi <shruthidipali@gmail.com>
2012-06-14 09:40:58 +0530

    Shruthi | #0 | Removed form validation in login page. Converting to lowercase in js before submit.

--- whp-web/src/main/webapp/js/login.js
+++ whp-web/src/main/webapp/js/login.js
@@ -1,31 +1,11 @@
 $(document).ready(function() {
-    $.validator.addMethod('lowercase', function (value, element, param){
-        return !(/.[!,:,@,#,$,%,^,&,*,?,_,~]/.test(value)
-                || /[A-Z]/.test(value));
-    }, 'Only lower case allowed');
 
-    $('#loginForm').validate({
-        rules: {
-            j_username: {
-                required: true,
-                lowercase: true
-            },
-            j_password:  {
-                required: true
-            }
-        },
-        messages: {
-            j_username: {
-                required: "Username cannot be empty",
-                lowercase: "Username cannot have upper case or special characters."
-            },
-            j_password: {
-                required: "Password cannot be empty"
-            }
-        },
-        errorPlacement: function(error, element) {
-            $('#loginError').append(error);
-        },
-        errorLabelContainer: '#loginError'
+    $("#loginForm").submit(function() {
+
+        var username = $("#j_username").val();
+        $("#j_username").val(username.toLowerCase());
+        return true;
     });
+
+
 });
