4f8595735253c951fa0a32a0e723e664b816aad5
Balaji Narain <balajin@thoughtworks.com>
2012-06-20 10:19:42 +0530

    BalajiN | #0 | Making spring integration test abstract

--- whp-common/src/test/java/org/motechproject/common/utils/SpringIntegrationTest.java
+++ whp-common/src/test/java/org/motechproject/common/utils/SpringIntegrationTest.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-public class SpringIntegrationTest {
+public abstract class SpringIntegrationTest {
 
     @Rule
     public ExpectedException exceptionThrown = ExpectedException.none();
