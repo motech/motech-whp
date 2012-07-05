169f3fb9f71a86d16ec004ec0ae59e380f596da2
Devaradhan <devaradhan@gmail.com>
2012-07-05 15:23:10 +0530

    <Dev> #000 | fixing hide/show search pane header in list patients page

--- whp-web/src/main/webapp/js/listPatients.js
+++ whp-web/src/main/webapp/js/listPatients.js
@@ -7,14 +7,12 @@ function initializeSearchPane() {
         collapsible:true,
         icons:icons,
         change:function (event, ui) {
-            if (ui.options.active == 0)
-                $('#search-section-header').text("Show Search Pane");
-            else
+            if (typeof ui.options.active == "number")
                 $('#search-section-header').text("Hide Search Pane");
+            else
+                $('#search-section-header').text("Show Search Pane");
         }
     });
-    $('#search-section').accordion("option", "active", true);
-
 }
 $(function () {
     initializeSearchPane();
