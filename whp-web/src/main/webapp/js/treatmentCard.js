03de9724e6d0af91c4c18f4370e0d6ef505ddea1
Devaradhan <devaradhan@gmail.com>
2012-07-05 15:00:20 +0530

    <Dev> #000 | fixing coloring provider in treatment card

--- whp-web/src/main/webapp/js/treatmentCard.js
+++ whp-web/src/main/webapp/js/treatmentCard.js
@@ -74,9 +74,10 @@ function colorCellsByProvider() {
 
     $('.editable').each(function () {
         if ($(this).attr('providerId') && $(this).attr('providerId') != "") {
-            if (!providerMap.hasOwnProperty($(this).attr('providerId')))
+            if (!providerMap.hasOwnProperty($(this).attr('providerId'))) {
                 providerMap[$(this).attr('providerId')] = providerColorCodes[colorPos];
-            colorPos = (colorPos + 1) % 10;
+                colorPos = (colorPos + 1) % 10;
+            }
         }
     });
 
