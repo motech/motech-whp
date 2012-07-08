ba2140b6d4126f637168eb826652cf6b6d69eb5f
Balaji Narain <balajin@thoughtworks.com>
2012-07-08 13:19:07 +0530

    BalajiN | #0 | Making next possible transitions a part of the enum

--- whp-refdata/src/main/java/org/motechproject/whp/refdata/domain/Phase.java
+++ whp-refdata/src/main/java/org/motechproject/whp/refdata/domain/Phase.java
@@ -1,13 +1,22 @@
 package org.motechproject.whp.refdata.domain;
 
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public enum Phase {
 
-    IP("Intensive Phase"), CP("Continuation Phase"), EIP("Extended Intensive Phase");
+    CP("Continuation Phase", Collections.<Phase>emptyList()),
+    EIP("Extended Intensive Phase", asList(CP)),
+    IP("Intensive Phase", asList(EIP, CP));
 
     private String name;
+    private final List<Phase> possibleTransitions;
 
-    Phase(String name) {
+    Phase(String name, List<Phase> possibleTransitions) {
         this.name = name;
+        this.possibleTransitions = possibleTransitions;
     }
 
     @Override
