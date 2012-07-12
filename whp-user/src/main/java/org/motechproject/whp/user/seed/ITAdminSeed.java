734dca170d65f579f9b30fb187a5eb3cb4812432
shruthi <shruthidipali@gmail.com>
2012-07-12 15:55:37 +0530

    Shruthi, Abhishek | #0 | Versioning seed data

--- whp-user/src/main/java/org/motechproject/whp/user/seed/ITAdminSeed.java
+++ whp-user/src/main/java/org/motechproject/whp/user/seed/ITAdminSeed.java
@@ -4,22 +4,21 @@ package org.motechproject.whp.user.seed;
 import org.motechproject.deliverytools.seed.Seed;
 import org.motechproject.security.exceptions.WebSecurityException;
 import org.motechproject.security.service.MotechAuthenticationService;
-import org.motechproject.whp.user.domain.CmfAdmin;
-import org.motechproject.whp.user.service.CmfAdminService;
+import org.motechproject.whp.user.domain.WHPRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
-public class AdministratorSeed {
+public class ITAdminSeed {
 
     @Autowired
     private MotechAuthenticationService authenticationService;
-    @Autowired
-    private CmfAdminService cmfAdminService;
 
-    @Seed(priority = 0, version = "1.0")
+    @Seed(priority = 0, version = "2.0")
     public void load() throws WebSecurityException {
-        cmfAdminService.add(new CmfAdmin("admin", "password","test","Delhi","Cmf Admin1"),"password");
+        authenticationService.register("itadmin1", "password", null, Arrays.asList(WHPRole.IT_ADMIN.name()));
+        authenticationService.register("itadmin2", "password", null, Arrays.asList(WHPRole.IT_ADMIN.name()));
     }
-
 }
