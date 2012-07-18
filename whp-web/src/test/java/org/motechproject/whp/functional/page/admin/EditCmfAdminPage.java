83d5dd2ff3a0114712d611c3a5e689b02feeb38c
Balaji Narain <balajin@thoughtworks.com>
2012-07-18 12:29:48 +0530

    BalajiN, Abhishek | #0 | Fixing functional tests

--- whp-web/src/test/java/org/motechproject/whp/functional/page/admin/EditCmfAdminPage.java
+++ whp-web/src/test/java/org/motechproject/whp/functional/page/admin/EditCmfAdminPage.java
@@ -1,15 +1,16 @@
 package org.motechproject.whp.functional.page.admin;
 
 import org.motechproject.whp.functional.framework.MyPageFactory;
+import org.motechproject.whp.functional.framework.WHPWebElement;
 import org.motechproject.whp.functional.framework.WebDriverFactory;
 import org.motechproject.whp.functional.page.LoggedInUserPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
 
 import static junit.framework.Assert.assertEquals;
-import static org.motechproject.whp.functional.framework.WebDriverFactory.createWebElement;
 
 public class EditCmfAdminPage extends LoggedInUserPage {
 
@@ -36,36 +37,42 @@ public class EditCmfAdminPage extends LoggedInUserPage {
         return MyPageFactory.initElements(webDriver, EditCmfAdminPage.class);
     }
 
+    @Override
+    public void postInitialize() {
+        this.location = WebDriverFactory.createWebElement(location);
+        this.emailId = WebDriverFactory.createWebElement(emailId);
+        this.staffName = WebDriverFactory.createWebElement(staffName);
+        this.department = WebDriverFactory.createWebElement(department);
+        this.submitButton = WebDriverFactory.createWebElement(submitButton);
+    }
+
     public ListAllCmfAdminsPage updateCmfAdmin(String userId, String location, String emailId, String staffName, String department) {
         setEmailId(emailId);
         setStaffName(staffName);
         setDepartment(department);
         setLocation(location);
         submitButton.click();
-        waitForSuccess("Successfully updated cmf admin with user id "+userId);
+        waitForSuccess("Successfully updated cmf admin with user id " + userId);
         return ListAllCmfAdminsPage.getListAllCmfAdminsPage(webDriver);
     }
 
     private EditCmfAdminPage setDepartment(String department) {
-        this.department.clear();
         this.department.sendKeys(department);
         return this;
     }
 
     private EditCmfAdminPage setStaffName(String staffName) {
-        this.staffName.clear();
         this.staffName.sendKeys(staffName);
         return this;
     }
 
     private EditCmfAdminPage setEmailId(String emailId) {
-        this.emailId.clear();
         this.emailId.sendKeys(emailId);
         return this;
     }
 
     private EditCmfAdminPage setLocation(String location) {
-        this.location.sendKeys(location);
+        new Select(this.location).selectByValue(location);
         return this;
     }
 
@@ -73,6 +80,6 @@ public class EditCmfAdminPage extends LoggedInUserPage {
         assertEquals(emailId, this.emailId.getAttribute("value"));
         assertEquals(staffName, this.staffName.getAttribute("value"));
         assertEquals(department, this.department.getAttribute("value"));
-        assertEquals(location, this.location.getAttribute("value"));
+        assertEquals(location, new Select(this.location).getFirstSelectedOption().getAttribute("value"));
     }
 }
