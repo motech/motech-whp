a00afb22485491c3da28475ef64f68ff1eb70d41
Vishal Naik <vishalmanohar@gmail.com>
2012-07-19 19:28:58 +0530

    Vishal|Deva #0 Added exception error page and logging on controllers. Added javax.mail dependency for SMTPAppender in log4j.

--- whp-web/src/main/java/org/motechproject/whp/controller/ProviderController.java
+++ whp-web/src/main/java/org/motechproject/whp/controller/ProviderController.java
@@ -20,7 +20,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/providers")
-public class ProviderController {
+public class ProviderController extends BaseController{
 
     private ProviderService providerService;
     private AllDistricts allDistrictsCache;
