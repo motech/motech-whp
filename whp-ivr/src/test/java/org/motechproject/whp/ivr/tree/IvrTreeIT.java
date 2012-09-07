66864c0e76b6accba0c769df81a9df4afa91a077
Devaradhan Murugesan <devaradhan@gmail.com>
2012-09-07 11:05:14 +0530

    Dev | #0 | Adding test suite class for ivr it part tests

---
+++ whp-ivr/src/test/java/org/motechproject/whp/ivr/tree/IvrTreeIT.java
@@ -0,0 +1,14 @@
+package org.motechproject.whp.ivr.tree;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AdherenceCaptureITPart.class,
+        MenuRepetitionITPart.class,
+        ReportingITPart.class,
+        SummaryMessagesITPart.class
+})
+public class IvrTreeIT {
+}
