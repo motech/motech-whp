0ab81f358f4bd43599089678e4ea69e561e76231
DheerendraNath <dheerenn@thoughtworks.com>
2012-10-24 19:01:14 +0530

    Dheeru | #502 | Removing port number from reporting application URL - SIDE AFFECT: not environment-proof

--- whp-reporting/src/test/java/org/motechproject/whp/reporting/ReportingEventURLsIT.java
+++ whp-reporting/src/test/java/org/motechproject/whp/reporting/ReportingEventURLsIT.java
@@ -18,17 +18,17 @@ public class ReportingEventURLsIT {
 
     @Test
     public void shouldGetAdherenceCaptureURL(){
-        assertThat(reportingEventURLs.getAdherenceCallLogURL(), is("http://localhost:8080/whp-reports/adherence/callLogs"));
+        assertThat(reportingEventURLs.getAdherenceCallLogURL(), is("http://localhost/whp-reports/adherence/callLogs"));
     }
 
     @Test
     public void shouldGetCallLogMeasureURL(){
-        assertThat(reportingEventURLs.getCallLogURL(), is("http://localhost:8080/whp-reports/callLog/measure"));
+        assertThat(reportingEventURLs.getCallLogURL(), is("http://localhost/whp-reports/callLog/measure"));
     }
 
     @Test
     public void shouldGetFlashingLogMeasureURL(){
-        assertThat(reportingEventURLs.getFlashingLogURL(), is("http://localhost:8080/whp-reports/flashingLog/measure"));
+        assertThat(reportingEventURLs.getFlashingLogURL(), is("http://localhost/whp-reports/flashingLog/measure"));
     }
 
 }
