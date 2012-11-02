8f840fb7e7b6786dc3d62ef5801ecdc4c38e5d13
Vignesh R E <vignesh.littlemaster@gmail.com>
2012-11-02 17:36:35 +0530

    V2, Vishal | #548 | Added three more instance names to sputum tracking

--- whp-common/src/main/java/org/motechproject/whp/common/domain/SputumTrackingInstance.java
+++ whp-common/src/main/java/org/motechproject/whp/common/domain/SputumTrackingInstance.java
@@ -1,10 +1,10 @@
 package org.motechproject.whp.common.domain;
 
 public enum SputumTrackingInstance {
-    PreTreatment("Pre-treatment"), EndIP("End of Intensive Phase"), ExtendedIP("Extended IP"), TwoMonthsIntoCP("Two Months Into CP"), EndTreatment("End Treatment");
+    PreTreatment("Pre-treatment"), EndIP("End of Intensive Phase"), ExtendedIP("Extended IP"), TwoMonthsIntoCP("Two Months Into CP"),
+    EndTreatment("End Treatment"), TreatmentInterruption1("Treatment Interruption 1"), TreatmentInterruption2("Treatment Interruption 2"), TreatmentInterruption3("Treatment Interruption 3");
 
-    public static SputumTrackingInstance[] MAPPING_INSTANCES = {PreTreatment, EndIP, ExtendedIP, TwoMonthsIntoCP, EndTreatment};
-    public static SputumTrackingInstance[] IN_TREATMENT_INSTANCES = {EndIP, ExtendedIP, TwoMonthsIntoCP, EndTreatment};
+    public static SputumTrackingInstance[] IN_TREATMENT_INSTANCES = {EndIP, ExtendedIP, TwoMonthsIntoCP, EndTreatment, TreatmentInterruption1, TreatmentInterruption2, TreatmentInterruption3};
 
     private String displayText;
 
@@ -17,28 +17,28 @@ public enum SputumTrackingInstance {
     }
 
     public static SputumTrackingInstance getInstanceForValue(String value) {
-        for(SputumTrackingInstance instance : SputumTrackingInstance.values()) {
-            if(instance.getDisplayText().equalsIgnoreCase(value) || instance.name().equalsIgnoreCase(value))
+        for (SputumTrackingInstance instance : SputumTrackingInstance.values()) {
+            if (instance.getDisplayText().equalsIgnoreCase(value) || instance.name().equalsIgnoreCase(value))
                 return instance;
         }
         return null;
     }
 
     public static boolean isValidMappingInstance(String text) {
-        return isValid(text, MAPPING_INSTANCES);
+        return isValid(text);
     }
 
     public static SputumTrackingInstance getInstanceByName(String name) {
-        for(SputumTrackingInstance instance : SputumTrackingInstance.values()) {
-            if(instance.name().equalsIgnoreCase(name))
+        for (SputumTrackingInstance instance : SputumTrackingInstance.values()) {
+            if (instance.name().equalsIgnoreCase(name))
                 return instance;
         }
         return null;
     }
 
-    private static boolean isValid(String text, SputumTrackingInstance[] instances) {
-        for(SputumTrackingInstance sputumTrackingInstance : instances)
-            if(sputumTrackingInstance.getDisplayText().equalsIgnoreCase(text) || sputumTrackingInstance.name().equalsIgnoreCase(text))
+    private static boolean isValid(String text) {
+        for (SputumTrackingInstance sputumTrackingInstance : values())
+            if (sputumTrackingInstance.getDisplayText().equalsIgnoreCase(text) || sputumTrackingInstance.name().equalsIgnoreCase(text))
                 return true;
         return false;
     }
--- whp-container/src/test/java/org/motechproject/whp/container/domain/SputumTrackingInstanceTest.java
+++ whp-container/src/test/java/org/motechproject/whp/container/domain/SputumTrackingInstanceTest.java
@@ -1,9 +1,11 @@
 package org.motechproject.whp.container.domain;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.junit.Test;
 import org.motechproject.whp.common.domain.SputumTrackingInstance;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SputumTrackingInstanceTest {
     @Test
@@ -16,5 +18,18 @@ public class SputumTrackingInstanceTest {
     public void shouldGetInstanceForGivenName() {
         assertEquals(SputumTrackingInstance.PreTreatment, SputumTrackingInstance.getInstanceByName("PreTreatment"));
         assertEquals(SputumTrackingInstance.PreTreatment, SputumTrackingInstance.getInstanceByName("Pretreatment"));
+    }
+
+    @Test
+    public void shouldListAllInTreatmentInstances() {
+        SputumTrackingInstance[] instances = SputumTrackingInstance.IN_TREATMENT_INSTANCES;
+        assertEquals(7, instances.length);
+        assertTrue(ArrayUtils.contains(instances, SputumTrackingInstance.EndIP));
+        assertTrue(ArrayUtils.contains(instances, SputumTrackingInstance.ExtendedIP));
+        assertTrue(ArrayUtils.contains(instances, SputumTrackingInstance.TwoMonthsIntoCP));
+        assertTrue(ArrayUtils.contains(instances, SputumTrackingInstance.EndTreatment));
+        assertTrue(ArrayUtils.contains(instances, SputumTrackingInstance.TreatmentInterruption1));
+        assertTrue(ArrayUtils.contains(instances, SputumTrackingInstance.TreatmentInterruption2));
+        assertTrue(ArrayUtils.contains(instances, SputumTrackingInstance.TreatmentInterruption3));
     }
 }
