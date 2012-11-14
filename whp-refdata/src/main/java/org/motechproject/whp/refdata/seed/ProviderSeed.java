bb0cdb392dfdf9487037c2da961811b4d2e21663
Balaji Narain <balajin@thoughtworks.com>
2012-11-14 12:22:20 +0530

    V2, BalajiN | #452 | Adding container range for provider as a part of seed data

--- whp-refdata/src/main/java/org/motechproject/whp/refdata/seed/ProviderSeed.java
+++ whp-refdata/src/main/java/org/motechproject/whp/refdata/seed/ProviderSeed.java
@@ -3,6 +3,10 @@ package org.motechproject.whp.refdata.seed;
 
 import org.motechproject.deliverytools.seed.Seed;
 import org.motechproject.util.DateUtil;
+import org.motechproject.whp.containermapping.domain.ContainerRange;
+import org.motechproject.whp.containermapping.domain.ContainerRanges;
+import org.motechproject.whp.containermapping.domain.ProviderContainerMapping;
+import org.motechproject.whp.containermapping.service.ProviderContainerMappingService;
 import org.motechproject.whp.user.contract.ProviderRequest;
 import org.motechproject.whp.user.service.ProviderService;
 import org.motechproject.whp.user.service.UserService;
@@ -18,11 +22,26 @@ public class ProviderSeed {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ProviderContainerMappingService providerContainerMappingService;
+
     // Need not have this, till we create logins for acutal providers
     @Seed(priority = 0, version = "1.0")
     public void load() {
         providerService.registerProvider(new ProviderRequest("raj", "Begusarai", "1234567890", DateUtil.now()));
         userService.activateUser("raj");
+        createProviderContainerMapping();
+    }
+
+    private void createProviderContainerMapping() {
+        ProviderContainerMapping mapping = new ProviderContainerMapping();
+        ContainerRanges containerRanges = new ContainerRanges();
+
+        mapping.setProviderId("raj");
+        containerRanges.add(new ContainerRange(10000000000l, 20000000000l));
+
+        mapping.setContainerRanges(containerRanges);
+        providerContainerMappingService.add(mapping);
     }
 
 }
