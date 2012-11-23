f16ced81028a04ac90724ad7139cb7748d24024a
Vishal Naik <vishalnaik@thoughtworks.com>
2012-11-23 22:17:02 +0530

    Vishal #0 updated whp-reports url #security

--- whp-reporting/src/test/java/org/motechproject/whp/reporting/ReportingEventURLsIT.java
+++ whp-reporting/src/test/java/org/motechproject/whp/reporting/ReportingEventURLsIT.java
@@ -18,51 +18,51 @@ public class ReportingEventURLsIT {
 
     @Test
     public void shouldGetAdherenceCaptureURL() {
-        assertThat(reportingEventURLs.getAdherenceCallLogURL(), is("http://localhost:8080/whp-reports/adherence/callLogs"));
+        assertThat(reportingEventURLs.getAdherenceCallLogURL(), is("http://127.0.0.1:9999/whp-reports/adherence/callLogs"));
     }
 
     @Test
     public void shouldGetCallLogMeasureURL() {
-        assertThat(reportingEventURLs.getCallLogURL(), is("http://localhost:8080/whp-reports/adherenceCallLog/measure"));
+        assertThat(reportingEventURLs.getCallLogURL(), is("http://127.0.0.1:9999/whp-reports/adherenceCallLog/measure"));
     }
 
     @Test
     public void shouldGetFlashingLogMeasureURL() {
-        assertThat(reportingEventURLs.getFlashingLogURL(), is("http://localhost:8080/whp-reports/flashingLog/measure"));
+        assertThat(reportingEventURLs.getFlashingLogURL(), is("http://127.0.0.1:9999/whp-reports/flashingLog/measure"));
     }
 
     @Test
     public void shouldGetContainerRegistrationLogMeasureURL() {
-        assertThat(reportingEventURLs.getContainerRegistrationLogURL(), is("http://localhost:8080/whp-reports/sputumTracking/containerRegistrationMeasure"));
+        assertThat(reportingEventURLs.getContainerRegistrationLogURL(), is("http://127.0.0.1:9999/whp-reports/sputumTracking/containerRegistrationMeasure"));
     }
 
     @Test
     public void shouldGetSputumLabResultsCaptureMeasureURL() {
-        assertThat(reportingEventURLs.getSputumLabResultsCaptureLogURL(), is("http://localhost:8080/whp-reports/sputumTracking/sputumLabResultsMeasure"));
+        assertThat(reportingEventURLs.getSputumLabResultsCaptureLogURL(), is("http://127.0.0.1:9999/whp-reports/sputumTracking/sputumLabResultsMeasure"));
     }
 
     @Test
     public void shouldGetContainerStatusUpdateMeasureURL() {
-        assertThat(reportingEventURLs.getContainerStatusUpdateLogURL(), is("http://localhost:8080/whp-reports/sputumTracking/containerStatusMeasure"));
+        assertThat(reportingEventURLs.getContainerStatusUpdateLogURL(), is("http://127.0.0.1:9999/whp-reports/sputumTracking/containerStatusMeasure"));
     }
 
     @Test
     public void shouldGetContainerPatientMappingMeasureURL() {
-        assertThat(reportingEventURLs.getContainerPatientMappingLogURL(), is("http://localhost:8080/whp-reports/sputumTracking/containerPatientMappingMeasure"));
+        assertThat(reportingEventURLs.getContainerPatientMappingLogURL(), is("http://127.0.0.1:9999/whp-reports/sputumTracking/containerPatientMappingMeasure"));
     }
 
     @Test
     public void shouldGetContainerRegistrationCallLogURL() {
-        assertThat(reportingEventURLs.getContainerRegistrationCallDetailsLogURL(), is("http://localhost:8080/whp-reports/containerRegistrationCallLog/updateCallDetails"));
+        assertThat(reportingEventURLs.getContainerRegistrationCallDetailsLogURL(), is("http://127.0.0.1:9999/whp-reports/containerRegistrationCallLog/updateCallDetails"));
     }
 
     @Test
     public void shouldGetProviderVerificationLogURL() {
-        assertThat(reportingEventURLs.getProviderVerificationLogURL(), is("http://localhost:8080/whp-reports/containerRegistrationCallLog/providerVerification"));
+        assertThat(reportingEventURLs.getProviderVerificationLogURL(), is("http://127.0.0.1:9999/whp-reports/containerRegistrationCallLog/providerVerification"));
     }
 
     @Test
     public void shouldGetContainerVerificationLogURL() {
-        assertThat(reportingEventURLs.getContainerVerificationLogURL(), is("http://localhost:8080/whp-reports/containerRegistrationCallLog/containerVerification"));
+        assertThat(reportingEventURLs.getContainerVerificationLogURL(), is("http://127.0.0.1:9999/whp-reports/containerRegistrationCallLog/containerVerification"));
     }
 }
