310039ae2a964ff28c9167491042ce4f216f5073
Vishal Naik <vishalnaik@thoughtworks.com>
2012-12-05 15:04:24 +0530

    Vishal #0 removing unused constants

--- whp-container/src/main/java/org/motechproject/whp/container/WHPContainerConstants.java
+++ whp-container/src/main/java/org/motechproject/whp/container/WHPContainerConstants.java
@@ -2,10 +2,6 @@ package org.motechproject.whp.container;
 
 public class WHPContainerConstants {
 
-    // Subjects
-    public static final String CONTAINER_ADDED_SUBJECT = "org.motechproject.whp.container.added";
-    public static final String CONTAINER_UPDATED_SUBJECT = "org.motechproject.whp.container.updated";
-
     public static final String TB_NEGATIVE_CODE = "1";
     public static final String CLOSURE_DUE_TO_MAPPING = "0";
 }
