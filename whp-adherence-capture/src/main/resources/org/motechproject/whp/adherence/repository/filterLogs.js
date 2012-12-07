26357f76fd58225dd62d9a5c1ca6a65675bc9597
Balaji Narain <balajin@thoughtworks.com>
2012-12-07 11:40:07 +0530

    BalajiN | #573 | fixing the search in the query parameters in the list function

--- whp-adherence-capture/src/main/resources/org/motechproject/whp/adherence/repository/filterLogs.js
+++ whp-adherence-capture/src/main/resources/org/motechproject/whp/adherence/repository/filterLogs.js
@@ -2,8 +2,9 @@ function(doc, req) {
     start({'headers': {'Content-Type': 'application/json'}});
     send('{"rows": [');
     var i = 0;
+    var providers = JSON.parse(req.query['providers']);
     while (row = getRow()) {
-        if(row.doc.type === 'AdherenceLog' && req.query['providers'].indexOf(row.doc.meta.PROVIDER_ID) != -1){
+        if(row.doc.type === 'AdherenceLog' && providers.indexOf(row.doc.meta.PROVIDER_ID) !== -1){
             var text = ((i===0)? "" : ",") + ('{"value" : "' + row.doc.meta.PROVIDER_ID + '"}')
             send(text);
         }
