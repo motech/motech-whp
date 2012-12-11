dfb389cd35cb28a01dfe5bf6cf5744bd6083417b
Vignesh R E <vignesh.littlemaster@gmail.com>
2012-12-11 13:14:04 +0530

    BalajiN | #0 | Adding master data for field staff

--- whp-refdata/src/main/java/org/motechproject/whp/refdata/seed/FieldStaffSeed.java
+++ whp-refdata/src/main/java/org/motechproject/whp/refdata/seed/FieldStaffSeed.java
@@ -2,12 +2,10 @@ package org.motechproject.whp.refdata.seed;
 
 
 import org.motechproject.deliverytools.seed.Seed;
-import org.motechproject.security.exceptions.WebSecurityException;
 import org.motechproject.security.service.MotechAuthenticationService;
 import org.motechproject.whp.common.exception.WHPErrorCode;
 import org.motechproject.whp.common.exception.WHPRuntimeException;
 import org.motechproject.whp.user.domain.WHPRole;
-import org.motechproject.whp.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,8 +19,44 @@ public class FieldStaffSeed {
 
     @Seed(priority = 0, version = "4.0")
     public void load(){
-        registerFieldStaff("Begusarai");
+        registerFieldStaff("Vaishali");
+        registerFieldStaff("Nalanda");
+        registerFieldStaff("Samastipur");
+        registerFieldStaff("Gopalganj");
+        registerFieldStaff("Patna");
+        registerFieldStaff("East Champaran");
+        registerFieldStaff("Muzaffarpur");
+        registerFieldStaff("West Champaran");
+        registerFieldStaff("Khagaria");
+        registerFieldStaff("Saharsa");
         registerFieldStaff("Jehanabad");
+        registerFieldStaff("Begusarai");
+        registerFieldStaff("Bhagalpur");
+        registerFieldStaff("Saran");
+        registerFieldStaff("Siwan");
+        registerFieldStaff("Darbhanga");
+        registerFieldStaff("Madhubani");
+        registerFieldStaff("Sitamarhi");
+        registerFieldStaff("Purnea");
+        registerFieldStaff("Gaya");
+        registerFieldStaff("Araria");
+        registerFieldStaff("Arwal");
+        registerFieldStaff("Madhepura");
+        registerFieldStaff("Aurangabad");
+        registerFieldStaff("Monghyr");
+        registerFieldStaff("Banka");
+        registerFieldStaff("Nawada");
+        registerFieldStaff("Bhojpur");
+        registerFieldStaff("Buxar");
+        registerFieldStaff("Rohtas");
+        registerFieldStaff("Jamui");
+        registerFieldStaff("Shiekhpura");
+        registerFieldStaff("Sheohar");
+        registerFieldStaff("Kaimur");
+        registerFieldStaff("Katihar");
+        registerFieldStaff("Supaul");
+        registerFieldStaff("Kishanganj");
+        registerFieldStaff("Lakhisarai");
     }
 
     private void registerFieldStaff(String district){
