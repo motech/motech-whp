5b0ed0c899c1c00b99f2a8d2046e97d25415c876
Vishal Naik <vishalnaik@thoughtworks.com>
2013-05-08 11:22:41 +0530

    Vishal #0 Logging WGN outbound JSON requests as well.

--- whp-wgn-outbound/src/main/java/org/motechproject/whp/wgn/outbound/service/WGNGateway.java
+++ whp-wgn-outbound/src/main/java/org/motechproject/whp/wgn/outbound/service/WGNGateway.java
@@ -1,14 +1,15 @@
 package org.motechproject.whp.wgn.outbound.service;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.motechproject.http.client.domain.EventCallBack;
 import org.motechproject.http.client.service.HttpClientService;
 import org.motechproject.whp.wgn.outbound.WGNRequest;
-import org.motechproject.whp.wgn.outbound.logging.WGNRequestLogger;
 import org.motechproject.whp.wgn.outbound.properties.WGNGatewayProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
 
@@ -31,17 +32,28 @@ public class WGNGateway {
     public void post(String url, WGNRequest request) {
         HashMap<String, String> headers = new HashMap<>();
         headers.put(wgnGatewayProperties.getApiKeyName(), wgnGatewayProperties.getApiKeyValue());
-        httpClientService.post(url, request.toXML(), headers);
-        log(url, request);
+        String requestXML = request.toXML();
+        httpClientService.post(url, requestXML, headers);
+        log(url, requestXML);
     }
 
     public void post(String url, Serializable request, EventCallBack eventCallBack) {
         HashMap<String, String> headers = new HashMap<>();
         headers.put(wgnGatewayProperties.getApiKeyName(), wgnGatewayProperties.getApiKeyValue());
         httpClientService.post(url, request, headers, eventCallBack);
+        log(url, toJson(request));
     }
 
-    private void log(String url, WGNRequest request) {
-        new WGNRequestLogger(logger).log(url, request);
+    private String toJson(Serializable request) {
+        try {
+            return new ObjectMapper().writeValueAsString(request);
+        } catch (IOException e) {
+            logger.error("Error occurred converting to json");
+            return "Error occurred converting to json";
+        }
+    }
+
+    private void log(String url, String request) {
+        logger.info(url + " " + request);
     }
 }
